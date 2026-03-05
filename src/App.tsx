96bf9b5f6338e8a737caed7928e3032f4082c188
Max Zakharzhevskiy <maxzz@msn.com>
2026-03-04 19:59:14 -0800

    WIP. initial clean up. 4. connect shadcn

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export function App() {
-    return (<>
-        <div className="text-red-500">
-            <h1>Hello World</h1>
-        </div>
-    </>);
-}
---
+++ src/components/0-app/0-app.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../ui/shadcn/button";
+
+export function App() {
+    return (<>
+        <div>
+            <h1 className="text-red-500">Hello World</h1>
+            <Button variant="outline">Test</Button>
+        </div>
+    </>);
+}
