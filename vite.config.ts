839936afb818d77080ec5d2bcef6d363ee401f41
Max Zakharzhevskiy <maxzz@msn.com>
2026-03-04 19:35:03 -0800

    WIP. initial clean up. 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     server: {
         port: 3000,
-        host: true,
     },
-    plugins: [react()],
+    plugins: [
+        react(),
+    ],
 });
