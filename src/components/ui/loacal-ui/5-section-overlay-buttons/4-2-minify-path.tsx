f44ed30b73056ebff2e6bc62a34ea328e06b922a
Max Zakharzhevskiy <maxzz@msn.com>
2026-04-13 20:54:31 -0700

    WIP. fix section path input minify

--- src/components/ui/loacal-ui/5-section-overlay-buttons/4-2-minify-path.tsx
+++ src/components/ui/loacal-ui/5-section-overlay-buttons/4-2-minify-path.tsx
@@ -1,12 +1,15 @@
+import { useSetAtom } from "jotai";
 import { useSnapshot } from "valtio";
 import { classNames } from "@/utils";
 import { appSettings } from "@/store/0-ui-settings";
+import { doNormalizePathAtom } from "@/store/0-atoms/2-4-0-editor-actions";
 import { IconSizeBigger, IconSizeSmaller } from "@/components/ui/icons/app-specific";
 import { Button } from "@/components/ui/shadcn/button";
 import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/shadcn/tooltip";
 
 export function OverlayButton_MinifyPath() {
     const { minifyOutput } = useSnapshot(appSettings.pathEditor);
+    const doNormalize = useSetAtom(doNormalizePathAtom);
     const label = `Now ${minifyOutput ? "Minifying" : "Expanding"}. Set ${minifyOutput ? "Expanding" : "Minifying"}`;
     
     return (
@@ -16,7 +19,10 @@ export function OverlayButton_MinifyPath() {
                     className={classNames("size-5")}
                     variant="ghost"
                     size="icon"
-                    onClick={() => appSettings.pathEditor.minifyOutput = !minifyOutput}
+                    onClick={() => {
+                        appSettings.pathEditor.minifyOutput = !minifyOutput;
+                        doNormalize();
+                    }}
                     aria-label={label}
                     title={label}
                     type="button"
