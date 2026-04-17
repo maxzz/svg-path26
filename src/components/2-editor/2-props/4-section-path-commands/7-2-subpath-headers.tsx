77b6e47a284695986547aa1da537bb8f3352534f
Max Zakharzhevskiy <maxzz@msn.com>
2026-04-16 21:48:28 -0700

    WIP. sub-paths. headeers as accordion. 7. update accordion trigger layout

--- src/components/2-editor/2-props/4-section-path-commands/7-2-subpath-headers.tsx
+++ src/components/2-editor/2-props/4-section-path-commands/7-2-subpath-headers.tsx
@@ -4,6 +4,7 @@ import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/
 import { Switch } from "@/components/ui/shadcn/switch";
 import { cn } from "@/utils";
 import { allSubPathsEnabledAtom, subPathEnabledAtom } from "@/store/0-atoms/2-0-svg-model";
+import { ChevronDown } from "lucide-react";
 
 export function CompoundPathToggleRow() {
     const [allEnabled, setAllEnabled] = useAtom(allSubPathsEnabledAtom);
@@ -27,15 +28,17 @@ export function SubPathToggleRow({ subPathIndex, children }: { subPathIndex: num
 
     return (
         <AccordionItem value={value} className="border-none">
-            <div className="px-1.5 py-1 w-full! text-[10px] text-muted-foreground flex items-center justify-between gap-x-2">
+            <div className="px-1.5 py-1 text-[10px] text-muted-foreground flex items-center justify-between gap-x-2 [&>h3]:w-full">
 
-                <AccordionTrigger className={triggerClasses}>
+                <AccordionTrigger className={triggerClasses} showIcon={false}>
 
                     <span className="shrink-0">
                         Subpath {subPathIndex + 1}
                     </span>
 
                     <div className="flex-1 h-px bg-linear-to-r from-slate-500/10 via-slate-500/50 to-slate-500/10" />
+
+                    <ChevronDown className="mr-1 size-3.5 shrink-0 text-muted-foreground transition-transform duration-200" />
 
                     <span className="shrink-0" onClick={(event) => event.stopPropagation()} onPointerDown={(event) => event.stopPropagation()}>
                         <Switch
@@ -57,4 +60,4 @@ export function SubPathToggleRow({ subPathIndex, children }: { subPathIndex: num
     );
 }
 
-const triggerClasses = "flex-1 w-full! min-w-0 py-0 px-0 h-auto text-[10px] text-muted-foreground hover:no-underline cursor-pointer [&[data-state=open]>svg]:rotate-180";
+const triggerClasses = "11 flex-1 min-w-0 py-0 px-0 h-auto text-[10px] text-muted-foreground hover:no-underline cursor-pointer [&[data-state=open]>svg]:rotate-180";
