117340056d19eddab270d7323a4888209c9fb8f9
Max Zakharzhevskiy <maxzz@msn.com>
2026-04-18 19:19:31 -0700

    WIP. exportSvgToFile. localize atoms. 3

--- src/components/4-dialogs/5-export-path/8-dialog-export-atoms.ts
+++ src/components/4-dialogs/5-export-path/8-dialog-export-atoms.ts
@@ -5,18 +5,25 @@ import { svgPathInputAtom } from "../../../store/0-atoms/1-1-svg-path-input";
 import { pathViewBoxAtom } from "../../../store/0-atoms/2-2-path-viewbox";
 import { computeExportViewBox } from "@/components/2-editor/2-props/4-section-path-commands/8-svg-utils";
 
-export type ExportViewBoxDraft = ViewBox;
+// Open dialog atom
 
 const exportSvgDialogOpenBaseAtom = atom(false);
 
-export const exportViewBoxDraftAtom = atom<ExportViewBoxDraft>([0, 0, 1, 1]);
+export const exportSvgDialogOpenAtom = atom(
+    (get) => get(exportSvgDialogOpenBaseAtom),
+    (_get, set, open: boolean) => {
+        if (open) {
+            set(doResetExportViewBoxDraftAtom);
+        }
+        set(exportSvgDialogOpenBaseAtom, open);
+    },
+);
 
 export const doResetExportViewBoxDraftAtom = atom(
     null,
     (get, set) => {
         const { exportStroke, exportStrokeWidth } = appSettings.export;
         const fallback = get(pathViewBoxAtom);
-
         set(
             exportViewBoxDraftAtom,
             computeExportViewBox(
@@ -28,12 +35,8 @@ export const doResetExportViewBoxDraftAtom = atom(
     },
 );
 
-export const exportSvgDialogOpenAtom = atom(
-    (get) => get(exportSvgDialogOpenBaseAtom),
-    (_get, set, open: boolean) => {
-        if (open) {
-            set(doResetExportViewBoxDraftAtom);
-        }
-        set(exportSvgDialogOpenBaseAtom, open);
-    },
-);
+// Local state atoms
+
+export type ExportViewBoxDraft = ViewBox;
+
+export const exportViewBoxDraftAtom = atom<ExportViewBoxDraft>([0, 0, 1, 1]);
